d9782aa8d4200dfc15f1aecb5e8b14c646ca1112
SRT DEV <sahirruiz4@gmail.com>
2025-11-24 22:35:11 -0500

    agregar rutas para los componentes de inicio, inicio de sesión y registro con protección de autenticación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,28 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
     loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then((m) => m.RegisterComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
