a744ea9168daf7ae4ba3c13d47f22d406a53d0a9
SRT DEV <sahirruiz4@gmail.com>
2025-11-27 11:33:52 -0500

    actualizar configuración de entorno de producción para usar variables de entorno de Netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,13 @@
+// This file will be replaced during build with environment variables from Netlify
+declare const process: any;
+
 export const environment = {
   production: true,
-  token: process.env['NG_APP_TOKEN'] || '',
+  token: 'DvvuM3B99JV1IQ7cDc15OvDJ6Xhekjqa',
 
   supabase: {
-    url: process.env['NG_APP_SUPABASE_URL'] || '',
-    anonKey: process.env['NG_APP_SUPABASE_ANON_KEY'] || '',
+    url: 'https://kuwyukqbinzurtxoxgbp.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1d3l1a3FiaW56dXJ0eG94Z2JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMjEyMTQsImV4cCI6MjA3OTU5NzIxNH0.R6toIrsQAJENnekd522xqAs6R8mtVKGM_3KqjR9LzOI',
     enabled: true,
   },
 };
